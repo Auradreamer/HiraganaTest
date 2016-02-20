bd84004d9ffdc9ea3c83603a219519420324de3a
Auradreamer <wangmengyu@wangmengyus-MacBook-Air.local>
2016-02-20 12:07:39 -0500

    test1 commits

--- app/src/main/java/com/example/wangmengyu/hiraganatest/SecondActivity.java
+++ app/src/main/java/com/example/wangmengyu/hiraganatest/SecondActivity.java
@@ -1,26 +1,23 @@
 package com.example.wangmengyu.hiraganatest;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button test1;
-    Button test2;
-
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.test_1);
+
+        Intent intent = getIntent();
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -32,30 +29,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-        test1 = (Button) findViewById(R.id.btn1);
-        test1();
-    }
-
-
-    public void test1() {
-
-        test1.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent();
-                        intent.setClass(MainActivity.this, SecondActivity.class);
-                        MainActivity.this.startActivity(intent);
-
-
-                    }
-                }
-
-
-        );
-
-
     }
 
 }
