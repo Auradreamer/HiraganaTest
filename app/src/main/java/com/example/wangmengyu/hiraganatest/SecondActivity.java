b9e0d6f49aabc6bd351318d946283f5bfedf4a86
Auradreamer <wangmengyu@wangmengyus-MacBook-Air.local>
2016-02-20 13:09:21 -0500

    test app with a single japanese character

--- app/src/main/java/com/example/wangmengyu/hiraganatest/SecondActivity.java
+++ app/src/main/java/com/example/wangmengyu/hiraganatest/SecondActivity.java
@@ -7,13 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView JapaneseChar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_1);
+
+        JapaneseChar = (TextView)findViewById(R.id.Japanesecharr);
+        JapaneseChar.setText("あ");
 
 
 
